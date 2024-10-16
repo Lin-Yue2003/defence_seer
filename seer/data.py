f5fc68f9be7d05d20f4479334c77620791f74ce1
Yue <pt921222@gmail.com>
2024-10-16 08:31:28 +0800

    change dataset

--- seer/data.py
+++ seer/data.py
@@ -303,8 +303,10 @@ def datasets_Cifar10():
 def filter_dataset_by_brightness(dataset, percent_remove=25):
     brightness_values = []
     datapoints = [dataset[i][0].unsqueeze(0) for i in range(len(dataset))]
+    
     for datapoint in datapoints:
-        brightness_values.append(property_scores(datapoint,'bright'))
+        brightness = property_scores(datapoint, 'bright')
+        brightness_values.append(brightness.item() if isinstance(brightness, torch.Tensor) else brightness)
     
     threshold = np.percentile(brightness_values, percent_remove)
     filtered_dataset = [(img, label) for (img, label), brightness in zip(dataset, brightness_values) if brightness > threshold]
